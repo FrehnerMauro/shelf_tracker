d8b288d53981445b4b010f6a6b122c57bbc93759
TheLemooon <luca.zweifel@stud.fhgr.ch>
2024-12-03 22:29:28 +0100

    added automatic Roi detection

--- Schrank_detektor.py
+++ Schrank_detektor.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import random
 from ultralytics import YOLO
 
 # threshold personen ekrennen
@@ -11,21 +13,29 @@ model = YOLO('yolov8m.pt')
 inventory = {"apple": 0, "bottle": 0}
 
 
-
+#parms for get Rois
+debugGetRois = False
+mult = 25 #for aproximate contours
+sollRoiCount = 4
+perimeterMin = 1300
+perimeterMax = 5000
+cannyThresholdLower = 30
+cannyThresholdUper = 60
 
 def __main__():
     
     frame_count = 0
     skip_frames = 4
+    getRoisSuccessfull = False
+    rois = []
     
     inventory = {"apple": 0, "bottle": 0}
-
     
     # Initialisiere das Bild
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("/Users/maurofrehner/Desktop/shelfV2.mp4")
+    #cap = cv2.VideoCapture("/Users/maurofrehner/Desktop/shelfV2.mp4")
     #cap = cv2.VideoCapture(r"C:\Users\marku\Documents\StudiumMobileRobotics\7.Semester\Bildverarbeitung2\Projekt\shelfV2.mp4")
-    #cap = cv2.VideoCapture("shelf1.mp4")
+    cap = cv2.VideoCapture("shelfV2.mp4")
     
     if not cap.isOpened():
         print("Fehler beim Zugriff auf die Webcam.")
@@ -53,73 +63,98 @@ def __main__():
         else:
             frame_count = 0  # Frame-Zähler zurücksetzen
 
-
-        
         # Verarbeite das aktuelle Frame
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
+        if not getRoisSuccessfull and isValid(frame):
+            getRoisSuccessfull, rois = getRoisOfShelfs(frame)  
+        elif getRoisSuccessfull:
+            # ROIs extrahieren
+            frame1, frame2, frame3, frame4 = extract_roi_from_frame(frame,rois)
 
-            
+            # YOLO-Verarbeitung
+            result1 = doYolo(frame1)
+            result2 = doYolo(frame2)
+            result3 = doYolo(frame3)
+            result4 = doYolo(frame4)
 
-
-        # ROIs extrahieren
-        frame1, frame2, frame3, frame4 = extract_roi_from_frame(frame)
-
-        # YOLO-Verarbeitung
-        result1 = doYolo(frame1)
-        result2 = doYolo(frame2)
-        result3 = doYolo(frame3)
-        result4 = doYolo(frame4)
-
-        # Ergebnisse zeichnen
-        drawObjects(result1, frame1)
-        drawObjects(result2, frame2)
-        drawObjects(result3, frame3)
-        drawObjects(result4, frame4)
+            # Ergebnisse zeichnen
+            drawObjects(result1, frame1)
+            drawObjects(result2, frame2)
+            drawObjects(result3, frame3)
+            drawObjects(result4, frame4)
         
                 # prüfe auf person
-        if isValid(frame):
-            inventory["apple"] = 0
-            inventory["bottle"] = 0
-            # Inventar aktualisieren
-            updateInventar(result1, inventory)
-            updateInventar(result2, inventory)
-            updateInventar(result3, inventory)
-            updateInventar(result4, inventory)
-        else:
-            # Schreibe Text ins Bild
-            cv2.putText(frame, "Person erkannt!", (150, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                        1, (0, 0, 255), 2, cv2.LINE_AA)
+            if isValid(frame):
+                inventory["apple"] = 0
+                inventory["bottle"] = 0
+                # Inventar aktualisieren
+                updateInventar(result1, inventory)
+                updateInventar(result2, inventory)
+                updateInventar(result3, inventory)
+                updateInventar(result4, inventory)
+            else:
+                # Schreibe Text ins Bild
+                cv2.putText(frame, "Person erkannt!", (150, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                            1, (0, 0, 255), 2, cv2.LINE_AA)
             
-        # Inventar anzeigen
-        draw_inventory_on_frame(inventory, frame)
+            # Inventar anzeigen
+            draw_inventory_on_frame(inventory, frame)
         
-        # Zeige das verarbeitete Bild und die ROIs an
-        cv2.imshow("YOLO Object Detection", frame)
-        cv2.imshow("Regal 1", frame1)
-        cv2.imshow("Regal 2", frame2)
-        cv2.imshow("Regal 3", frame3)
-        cv2.imshow("Regal 4", frame4)
+            # Zeige das verarbeitete Bild und die ROIs an
+            cv2.imshow("YOLO Object Detection", scaleImage(frame))
+            cv2.imshow("Regal 1", frame1)
+            cv2.imshow("Regal 2", frame2)
+            cv2.imshow("Regal 3", frame3)
+            cv2.imshow("Regal 4", frame4)
 
         # Beende das Programm bei Tastendruck
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-        
-    
 
     # Freigeben der Ressourcen
     cap.release()
     cv2.destroyAllWindows()
     
-    
-    
-    
-    
 def getRoisOfShelfs(frame):
-    blurred_image = cv2.GaussianBlur(frame, (5, 5), 0)
+    rois = []
+
     # Perform Canny edge detection
-    return cv2.Canny(blurred_image, threshold1=100, threshold2=200)
+    canny = cv2.Canny(frame, threshold1=cannyThresholdLower, threshold2=cannyThresholdUper)
+    kernel = np.ones((5,5),np.uint8)
+    canny = cv2.dilate(canny,kernel,iterations = 3)
+    canny = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+
+    all_contours, __ = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contourFrame = frame.copy()
+    for contour in all_contours:
+        # Approximate contour to a polygon
+        perimeter = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.002* mult * perimeter, True)
+
+        # Calculate aspect ratio and bounding box
+        if perimeter >= perimeterMin and perimeter <= perimeterMax and len(approx) >=4:
+            x,y,w,h = cv2.boundingRect(approx)
+            aspect_ratio = float(w) / h
+            if aspect_ratio > 1.5 and aspect_ratio < 2.8:
+                rois.append([x,y,w,h])
+                if debugGetRois:
+                    frame = cv2.rectangle(frame,(x,y),(x+w,y+h), color=(255,0,0),thickness=4)
+
+            if debugGetRois:
+                # Draw bounding box
+                cv2.drawContours(contourFrame, [approx], -1, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 3)
+                cv2.putText(contourFrame,"Rectangle",(x, y - 10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0, 255, 0),2,)
+
+    if debugGetRois:
+        cv2.imshow("Canny",scaleImage(canny))
+        cv2.imshow("contour",scaleImage(contourFrame))
+        cv2.imshow("roi",scaleImage(frame))
+        #cv2.waitKey(0)
+        
+    if len(rois) == sollRoiCount:
+        return True, rois 
+    return False, rois
 
 def isValid(frame):
     #person in frame?
@@ -186,13 +221,13 @@ def drawObjects(results, frame):
     
     
 
-def extract_roi_from_frame(frame):
+def extract_roi_from_frame(frame, rois):
 
     # manuelle ROI setzen
-    x1, y1, width1, height1 = 400, 520, 400, 150
-    x2, y2, width2, height2 = 400, 750, 400, 150
-    x3, y3, width3, height3 = 400, 1000, 400, 130
-    x4, y4, width4, height4 = 400, 1250, 400, 150
+    x1, y1, width1, height1 = rois[0][0], rois[0][1], rois[0][2], rois[0][3]
+    x2, y2, width2, height2 = rois[1][0], rois[1][1], rois[1][2], rois[1][3]
+    x3, y3, width3, height3 = rois[2][0], rois[2][1], rois[2][2], rois[2][3]
+    x4, y4, width4, height4 = rois[3][0], rois[3][1], rois[3][2], rois[3][3]
 
 
     # Überprüfen, ob die Koordinaten innerhalb des Frames liegen
@@ -221,7 +256,10 @@ def draw_inventory_on_frame(inventory, frame):
 
     return frame
 
-
+def scaleImage(img, scale=2.0):
+    width = int(img.shape[1]  / scale)
+    height = int(img.shape[0]  / scale)
+    return cv2.resize(img,(width, height))
     
 if __name__ == "__main__":
     __main__()
